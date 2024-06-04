608a570b81d43a292cea847ad94e65399d6fbbbc
bahadirgezer <bahadirgezerbg@gmail.com>
2024-06-04 19:55:09 +0300

    update title

--- observablehq.config.js
+++ observablehq.config.js
@@ -1,7 +1,7 @@
 // See https://observablehq.com/framework/config for documentation.
 export default {
   // The project’s title; used in the sidebar and webpage titles.
-  title: "Project X",
+  title: "Social Synapse",
 
   // The pages and sections in the sidebar. If you don’t specify this option,
   // all pages will be listed in alphabetical order. Listing pages explicitly
@@ -23,15 +23,15 @@ export default {
   root: "src",
 
   // Some additional configuration options and their defaults:
-  theme: "default", // try "light", "dark", "slate", etc.
+  theme: "air", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
-  footer: "Project X, 2024", // what to show in the footer (HTML)
+  footer: "The Social Synapse, 2024", // what to show in the footer (HTML)
   // sidebar: true, // whether to show the sidebar
   // toc: true, // whether to show the table of contents
   pager: false, // whether to show previous & next links in the footer
   // output: "dist", // path to the output root for build
-  // search: true, // activate search
+  search: true, // activate search
   // linkify: true, // convert URLs in Markdown to links
-  // typographer: false, // smart quotes and other typographic improvements
-  // cleanUrls: true, // drop .html from URLs
+  typographer: true, // smart quotes and other typographic improvements
+  cleanUrls: true, // drop .html from URLs
 };
